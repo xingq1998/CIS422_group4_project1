a60eb9b4e9a425c56945ba97ed38d3fd6b8c5e5b
xingq1998 <3326999990@qq.com>
2021-04-15 15:50:55 +0800

    add simple views

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: /users/
+    path('', views.index, name='index'),
+    # ex: /users/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /users/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /users/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
