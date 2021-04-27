68debba35c306e5c902910f8bf4687d893369713
xingq1998 <3326999990@qq.com>
2021-04-27 16:46:19 +0800

    update testdata uploading

--- app/models.py
+++ app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -10,7 +9,7 @@ from clinics.models import ScheduleTime
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     phone = models.CharField(max_length=20, blank=True)
-    appoint = models.ManyToManyField(to=ScheduleTime) 
+    appoint = models.ManyToManyField(to=ScheduleTime)
     is_scheduled = False
 
     def __str__(self):
--- uploadTestData.py
+++ uploadTestData.py
@@ -16,10 +16,10 @@ Usage:
 
 
 TODO: Fix this warning.
->>>>>>>>>
-RuntimeWarning: DateTimeField ScheduleTime.start_time received 
-a naive datetime (2021-04-26 08:00:00) while time zone support is active.
->>>>>>>>>>
+# >>>>>>>>>
+# RuntimeWarning: DateTimeField ScheduleTime.start_time received
+# a naive datetime (2021-04-26 08:00:00) while time zone support is active.
+# >>>>>>>>>>
 
 
 WARNING: The following removes all entries from tables. 
@@ -31,8 +31,10 @@ On command line: manage.py flush
 """
 # Need to setup the django enviornment, so the setting module can be located.
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")
 import django
+
 django.setup()
 
 import datetime
@@ -40,27 +42,40 @@ from enum import IntEnum
 from clinics.models import ScheduleTime, Clinic
 
 # CSV input file for clinic locations.
-CLINIC_INPUT_FILE = r"Test/input_clinics.csv"
+CLINIC_INPUT_FILE = r"Test/input_clinics_update.csv"
+
 
 class InputHeaders(IntEnum):
-    ADDRESS = 0
-    CITY = 1
-    STATE = 2
-    ZIPCODE = 3
-    PHIZER_STOCK = 4
-    MODERNA_STOCK = 5
+    Name = 0
+    ADDRESS = 1
+    CITY = 2
+    STATE = 3
+    ZIPCODE = 4
+    PHIZER_STOCK = 5
+    MODERNA_STOCK = 6
+    janssenStock = 7
+
 
 def createClinic(data_list):
     clinic_obj = Clinic.objects.create(
+        name=data_list[InputHeaders.Name],
         address=data_list[InputHeaders.ADDRESS],
         city=data_list[InputHeaders.CITY],
         state=data_list[InputHeaders.STATE],
         zip_code=data_list[InputHeaders.ZIPCODE],
         phizer_stock=int(data_list[InputHeaders.PHIZER_STOCK]),
-        moderna_stock=int(data_list[InputHeaders.MODERNA_STOCK])
+        moderna_stock=int(data_list[InputHeaders.MODERNA_STOCK]),
+        janssen_stock=int(data_list[InputHeaders.janssenStock]),
+        Children=int(data_list[8]),
+        Adults=int(data_list[9]),
+        Seniors=int(data_list[10]),
+        Others=int(data_list[11]),
+        All_Ages=int(data_list[12]),
+        pic_address=data_list[13],
     )
 
     return clinic_obj
+
 
 def createSchedule(num, clinic_obj):
     """ Create num ScheduleTime for clinic_obj. """
@@ -76,14 +91,15 @@ def createSchedule(num, clinic_obj):
 
     return None
 
+
 def main():
     with open(CLINIC_INPUT_FILE, "r") as fp:
         # Ignore the headers
         fp.readline()
-        
+
         for line in fp:
             data = line.rstrip().split(",")
-        
+
             clin_obj = createClinic(data)
 
             # Set these values to int type.
@@ -92,9 +108,9 @@ def main():
 
             createSchedule(data[InputHeaders.PHIZER_STOCK], clin_obj)
             createSchedule(data[InputHeaders.MODERNA_STOCK], clin_obj)
-        
 
     return None
+
 
 if __name__ == "__main__":
     main()
